6df544d3ca915e20a830a6946064f047893a7005
Drew Benson <drew+gh@adbenson.net>
2014-09-23 18:28:26 -0400

    Getting color from the user

--- src/cosc561/checkers/Checkers.java
+++ src/cosc561/checkers/Checkers.java
@@ -1,5 +1,8 @@
 package cosc561.checkers;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class Checkers {
 	
 	public static void main(String[] args) {
@@ -8,6 +11,29 @@ public class Checkers {
 	
 	public Checkers() {
 		
+		System.out.println(inquireColor());
+		
 	}
-
+	
+	private COLOR inquireColor() {
+		
+		int n = JOptionPane.showOptionDialog(new JFrame(),
+		    "What color are we playing as today?",
+		    "Choose our color",
+		    JOptionPane.YES_NO_CANCEL_OPTION,
+		    JOptionPane.QUESTION_MESSAGE,
+		    null,
+		    COLOR.values(),
+		    null);
+		
+		if (n < 0) {
+			throw new RuntimeException("No choice made");
+		}
+		
+		return COLOR.values()[n];
+	}
+	
+	public static enum COLOR {
+		RED, BLACK;
+	}
 }
