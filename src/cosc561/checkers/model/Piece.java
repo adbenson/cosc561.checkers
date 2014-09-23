51b94c41ea18185705b9fc1d6c27c42e671f30e4
Drew Benson <drew+gh@adbenson.net>
2014-09-23 18:57:45 -0400

    Adding color property and king flag to piece

--- src/cosc561/checkers/model/Piece.java
+++ src/cosc561/checkers/model/Piece.java
@@ -1,5 +1,23 @@
 package cosc561.checkers.model;
 
+import cosc561.checkers.Checkers.COLOR;
+
 public class Piece {
+		
+	public final COLOR color;
+	
+	private boolean king;
+	
+	public Piece(COLOR color) {
+		this.color = color;
+	}
+
+	public boolean isKing() {
+		return king;
+	}
+
+	public void setKing(boolean king) {
+		this.king = king;
+	}
 
 }
