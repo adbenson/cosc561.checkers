c770ae9bc1d54e21f0ba55122888b444dcc28f17
Drew Benson <drew+gh@adbenson.net>
2014-09-24 21:39:15 -0400

    Unit tests!

---
+++ src/cosc561/checkers/BoardTest.java
@@ -0,0 +1,100 @@
+package cosc561.checkers;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cosc561.checkers.model.Board;
+import cosc561.checkers.model.Board.Space;
+import cosc561.checkers.model.Piece;
+
+public class BoardTest {
+	
+	private Board board;
+	
+	@Before
+	public void setUp() {
+		board = new Board();
+	}
+
+	@Test
+	public void getLegalMovesEmptySpace() {
+		board.fill();
+		
+		List<Space> moves = board.getSpace(18).getLegalMoves();
+		
+		assertEquals(moves.size(), 0);
+	}
+	
+	@Test
+	public void getLegalMovesNoOpponents() {
+		board.fill();
+		
+		//RED
+		List<Space> moves = board.getSpace(22).getLegalMoves();
+		
+		assertEquals(moves.size(), 2);
+		
+		assertTrue(hasId(moves, 17));
+		assertTrue(hasId(moves, 18));
+		
+		//BLACK
+		moves = board.getSpace(10).getLegalMoves();
+		
+		assertEquals(moves.size(), 2);
+		
+		assertTrue(hasId(moves, 14));
+		assertTrue(hasId(moves, 15));
+	}
+	
+	@Test
+	public void getLegalMovesNoOpponentsEdge() {
+		board.fill();
+		
+		//RED
+		List<Space> moves = board.getSpace(21).getLegalMoves();
+		
+		assertEquals(moves.size(), 1);
+		assertTrue(hasId(moves, 17));
+		
+		//BLACK
+		moves = board.getSpace(12).getLegalMoves();
+		
+		assertEquals(moves.size(), 1);
+		assertTrue(hasId(moves, 16));
+	}
+	
+	@Test
+	public void getLegalMovesFriendlyBlocked() {
+		board.fill();
+		
+		//RED
+		List<Space> moves = board.getSpace(7).getLegalMoves();
+		
+		assertEquals(moves.size(), 0);
+	}
+	
+	@Test
+	public void getLegalMovesEnemyBlocked() {
+		board.fill();
+		
+		//RED
+		List<Space> moves = board.getSpace(7).getLegalMoves();
+		
+		assertEquals(moves.size(), 0);
+	}
+	
+	private boolean hasId(List<Space> list, int id) {
+		for(Space s : list) {
+			if (s.id == id) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+}
