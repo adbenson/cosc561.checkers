a0cd2dc19da852d14c34d3cfa97b1067d652b43d
Drew Benson <drew+gh@adbenson.net>
2014-09-26 07:52:17 -0400

    Moving logic for which spaces start as which colors to Piece class

--- src/cosc561/checkers/model/Piece.java
+++ src/cosc561/checkers/model/Piece.java
@@ -28,12 +28,26 @@ public class Piece {
 		RED (true, new Direction[] {NE, NW}), 
 		BLACK (false, new Direction[] {SE, SW}); 
 		
+		public static final int LAST_BLACK = 12;
+		public static final int FIRST_RED = 21;
+		
 		public final boolean first;
 		public final Direction[] directions;
 		
 		private Color(boolean first, Direction[] directions) {
 			this.first = first;
 			this.directions = directions;
+		}
+		
+		public static Color getColorForSpace(int id) {
+			if (id <= LAST_BLACK) {
+				return BLACK;
+			}
+			if (id >= FIRST_RED) {
+				return RED;
+			}
+			
+			return null;
 		}
 	}
 
@@ -48,5 +62,9 @@ public class Piece {
 
 	public boolean isOpponent(Piece that) {
 		return that.color != this.color;
+	}
+	
+	public String toString() {
+		return (color == Color.RED? "R" : "B") + (isKing()? "K" : " ");
 	}
 }
