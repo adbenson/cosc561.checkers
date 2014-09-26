45aeda2f89d4d412db93e9fb9b2bdda7c24abf42
Drew Benson <drew+gh@adbenson.net>
2014-09-26 07:53:49 -0400

    Refactoring for other changes

--- src/cosc561/checkers/model/Board.java
+++ src/cosc561/checkers/model/Board.java
@@ -1,50 +1,88 @@
 package cosc561.checkers.model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Board {
 
 	private static Grid grid = Grid.getInstance();
 
-	
-	
-	
-	
-	
-	private HashMap<Integer, Space> spaces;
+	private Piece[] pieces;
 	
 	public Board() {
-		initializeSpaces();
+		pieces = new Piece[Grid.USED_SPACES + 1];
 	}
 	
 	public void addStartingPieces() {
-		for(Space space : spaces.values()) {
-			if (space.id <= LAST_BLACK) {
-				space.piece = new Piece(Piece.Color.BLACK);
-			}
-			if (space.id >= FIRST_RED) {
-				space.piece = new Piece(Piece.Color.RED);
+		for(Space space : grid.getSpaces()) {
+			Piece.Color color = Piece.Color.getColorForSpace(space.id);
+			if (color != null) {
+				addPiece(space, new Piece(color));
 			}
 		}
 	}
 	
+	public void addPiece(int id, Piece piece) {
+		pieces[id] = piece;
 	}
 	
-	public Space getSpace(int i) {
-		return spaces.get(i);
+	public void addPiece(Space space, Piece piece) {
+		addPiece(space.id, piece);
 	}
 	
+	public Piece getPiece(Space space) {
+		return pieces[space.id];
+	}
+	
+	
+	}
+	
+	public Space getSpace(int id) {
+		return grid.getSpaceById(id);
+	}
+	
+	public boolean isEmpty(Space space) {
+		return pieces[space.id] == null;
+	}
+	
+	public List<Space> getLegalMoves(int id) {
+		return getLegalMoves(getSpace(id));
+	}
+	
+	public List<Space> getLegalMoves(Space space) {
+		Piece piece = getPiece(space);
+
+		List<Space> moves = new ArrayList<Space>();
+
+		if (piece != null) {
 			
+			for (Direction direction : piece.getDirections()) {
 				
+				Space candidate = grid.getAdjacent(space, direction);
+				
+				//Edge spaces won't have some adjacent spaces
+				if (candidate != null) {
 					
+					if (!isEmpty(candidate)) {
+						if (piece.isOpponent(getPiece(candidate))) {
+							Space target = grid.getAdjacent(candidate, direction);
+							if (target != null && isEmpty(target)) {
+								//Make the jump!
+								moves.add(target);
+							}
+							//Opponent piece is against a side, can't jump
+						}						
+						//Friendly piece, do nothing
+					}
+					else {
+						moves.add(candidate);
 					}
 				}
 			}
 		}
 		
+		return moves;
+	}
 	}
 
 }
