4a1223d9f854326db662e7c2767678fa0eaeeca2
Drew Benson <drew+gh@adbenson.net>
2014-09-30 21:29:00 -0400

    Moving board comparison logic to Evaluator parent

--- src/cosc561/checkers/evaluator/Evaluator.java
+++ src/cosc561/checkers/evaluator/Evaluator.java
@@ -1,5 +1,7 @@
 package cosc561.checkers.evaluator;
 
+import java.util.List;
+
 import cosc561.checkers.model.BoardState;
 import cosc561.checkers.model.PlayerColor;
 
@@ -12,5 +14,19 @@ public abstract class Evaluator {
 	}
 
 	public abstract int evaluate(BoardState state, PlayerColor currentPlayer);
+	
+	public BoardState chooseBest(List<BoardState> candidates, PlayerColor forPlayer) {
+		int bestScore = 0;
+
+		BoardState bestState = null;
+		
+		for (BoardState state : candidates) {
+			if (bestState == null || evaluate(state, forPlayer) > bestScore) {
+				bestState = state;
+			}
+		}
+		
+		return bestState;
+	}
 	
 }
