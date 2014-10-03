b29092ad45d54e09b825ca7ee7fb45548f79f123
Drew Benson <drew+gh@adbenson.net>
2014-10-03 00:28:26 -0400

    Some demo runner code

--- src/cosc561/checkers/Checkers.java
+++ src/cosc561/checkers/Checkers.java
@@ -3,18 +3,37 @@ package cosc561.checkers;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
+import cosc561.checkers.model.BoardState;
+import cosc561.checkers.model.Grid;
 import cosc561.checkers.model.PlayerColor;
 
 public class Checkers {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		new Checkers();
 	}
 	
-	public Checkers() {
+	public Checkers() throws Exception {
 		
-		System.out.println(inquireColor());
+		System.out.println(Grid.getInstance());
 		
+		PlayerColor color = PlayerColor.RED; //inquireColor();
+		
+		BoardState state = new BoardState(PlayerColor.startingPlayer).addStartingPieces();
+		System.out.println(state);
+		
+		Player player = new Player(color, 6);
+		Player opponent = new Player(color.opponent(), 6);
+		
+		while (true) {
+		
+			state = player.nextMove(state);
+			System.out.println(state);
+			
+			state = opponent.nextMove(state);
+			System.out.println(state);
+		}
+
 	}
 	
 	private PlayerColor inquireColor() {
