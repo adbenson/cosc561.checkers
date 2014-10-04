c0ea58a128bf5f7ad515c251d97b41cd38d847a6
Drew Benson <drew+gh@adbenson.net>
2014-10-04 19:17:48 -0400

    Made a test for the PieceMap iterators

---
+++ src/cosc561/checkers/test/PieceMapTest.java
@@ -0,0 +1,101 @@
+package cosc561.checkers.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cosc561.checkers.model.BoardState;
+import cosc561.checkers.model.Grid;
+import cosc561.checkers.model.Piece;
+import cosc561.checkers.model.PieceMap;
+import cosc561.checkers.model.PieceMap.Entry;
+import cosc561.checkers.model.PieceMap.IllegalMoveException;
+import cosc561.checkers.model.PlayerColor;
+
+public class PieceMapTest {
+	
+	BoardState board;
+	PieceMap pieces;
+	
+	Grid grid = Grid.getInstance();
+	
+	@Before
+	public void setUp() throws IllegalMoveException {
+		board = new BoardState(PlayerColor.RED).addStartingPieces();
+		pieces = board.getPieces();
+	}
+	
+	@Test
+	public void testIterateAll() {
+		int i=0;
+		
+		for (Piece piece : pieces) {
+			i++;
+		}
+		
+		assertEquals(24, i);
+	}
+	
+	@Test
+	public void testIterateRed() {
+		int i=0;
+		
+		for (Piece piece : pieces.iterate(PlayerColor.RED)) {
+			i++;
+		}
+		
+		assertEquals(12, i);
+	}
+	
+	@Test
+	public void testIteratePartialBoardRed() throws IllegalMoveException {
+		System.out.println(grid);
+		board.removePiece(grid.getSpaceById(21));
+		board.removePiece(grid.getSpaceById(10));
+		board.removePiece(grid.getSpaceById(24));
+		board.removePiece(grid.getSpaceById(31));
+		
+		int i=0;
+		
+		for (Piece piece : pieces.iterate(PlayerColor.RED)) {
+			i++;
+		}
+		
+		assertEquals(9, i);
+	}
+	
+	@Test
+	public void testIteratePartialBoardAll() throws IllegalMoveException {
+		System.out.println(grid);
+		board.removePiece(grid.getSpaceById(21));
+		board.removePiece(grid.getSpaceById(10));
+		board.removePiece(grid.getSpaceById(24));
+		board.removePiece(grid.getSpaceById(31));
+		
+		int i=0;
+		
+		for (Piece piece : pieces) {
+			i++;
+		}
+		
+		assertEquals(20, i);
+	}
+
+	@Test
+	public void testIteratePartialBoardSpaces() throws IllegalMoveException {
+		System.out.println(grid);
+		board.removePiece(grid.getSpaceById(21));
+		board.removePiece(grid.getSpaceById(10));
+		board.removePiece(grid.getSpaceById(24));
+		board.removePiece(grid.getSpaceById(31));
+		
+		int i=0;
+		
+		for (Entry piece : pieces.iterateSpaces(PlayerColor.RED)) {
+			i++;
+		}
+		
+		assertEquals(9, i);
+	}
+}
