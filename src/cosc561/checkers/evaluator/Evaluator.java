869e308c8917f337a39a2694cf0c697aa65e3b7b
Drew Benson <drew+gh@adbenson.net>
2014-10-05 01:33:52 -0400

    Added class for mapping score to state

--- src/cosc561/checkers/evaluator/Evaluator.java
+++ src/cosc561/checkers/evaluator/Evaluator.java
@@ -10,6 +10,40 @@ public abstract class Evaluator {
 	public Evaluator(PlayerColor playerColor) {
 		this.playerColor = playerColor;
 	}
+	public static class EvaluatedState implements Comparable<EvaluatedState> {
+		public final BoardState state;
+		private final int score;
+				
+		public EvaluatedState(BoardState state, int score) {
+			this.state = state;
+			this.score = score;
+		}
+
+		@Override
+		public int compareTo(EvaluatedState that) {
+			return this.score - that.score;
+		}
+		
+		public static EvaluatedState best(EvaluatedState s1, EvaluatedState s2) {
+			if (s1 == null) {
+				return s2;
+			}
+			if (s2 == null) {
+				return s1;
+			}
+			return (s1.score > s2.score)? s1 : s2;
+		}
+		
+		public static EvaluatedState worst(EvaluatedState s1, EvaluatedState s2) {
+			if (s1 == null) {
+				return s2;
+			}
+			if (s2 == null) {
+				return s1;
+			}
+			return (s1.score < s2.score)? s1 : s2;
+		}
+	}
 
 	public abstract int evaluate(BoardState state, PlayerColor currentPlayer);
 	
