5eb844bf3bc8a80fe6c5b861c3c91a19b3d44177
Drew Benson <drew+gh@adbenson.net>
2014-10-05 01:39:07 -0400

    NEGAMAX

--- src/cosc561/checkers/Player.java
+++ src/cosc561/checkers/Player.java
@@ -1,6 +1,11 @@
 package cosc561.checkers;
 
+import java.util.List;
+
 import cosc561.checkers.evaluator.Evaluator;
+import cosc561.checkers.evaluator.Evaluator.EvaluatedState;
+import cosc561.checkers.evaluator.EvaluatorManager;
+import cosc561.checkers.evaluator.PieceCountEvaluator;
 import cosc561.checkers.model.BoardState;
 import cosc561.checkers.model.PieceMap.IllegalMoveException;
 import cosc561.checkers.model.PlayerColor;
@@ -15,17 +20,54 @@ public class Player {
 	public Player(PlayerColor color, int searchDepth) {
 		this.color = color;
 		this.searchDepth = searchDepth;
+		
+		evaluator = new PieceCountEvaluator(color);
 	}
 
 	public BoardState nextMove(BoardState currentState) throws IllegalMoveException {
-
-		BoardState chosenState = currentState.getAllPossibleStates(color).get(0);
+		if (currentState.isEndgame()) {
+			System.err.println("nextMove called on completed game");
+			return currentState;
+		}
 		
-		System.out.println(this + " has made a move: " + "\n" + chosenState.printHistory());
+		EvaluatedState evaluated = evaluator.evaluate(currentState, color);
 		
-		return chosenState;
+		EvaluatedState chosenEvaluated = negamax(evaluated, searchDepth, color);
+		if (chosenEvaluated == null) {
+			System.out.println();
+		}
+		BoardState chosen = chosenEvaluated.state;
+		
+		BoardState play = chosen.getFirstUnplayed();
+		play.setPlayed();
+		
+		System.out.println(this + " has made a move: " + "\n" + play.printHistory());
+		
+		return play;
 	}
-
+	
+	public EvaluatedState negamax(EvaluatedState state, int depth, PlayerColor player) throws IllegalMoveException {
+		if (depth == 0) {
+			return state;
+		}
+				
+		List<BoardState> childStates = state.state.getAllPossibleStates(player);
+		
+		if (state.state.isEndgame()) {
+			return state;
+		}
+		
+		List<EvaluatedState> evaluated = evaluator.evaluate(childStates, player);
+		
+		EvaluatedState bestState = null;
+		for (EvaluatedState child : evaluated) {
+			EvaluatedState childResult = negamax(child, depth - 1, player.opponent());
+			bestState = EvaluatedState.worst(bestState, childResult);
+		}
+		
+		return bestState;
+	}
+	
 	public String toString() {
 		return color.toString();
 	}
